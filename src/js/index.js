dfa61644a7379d7de5423a6e11ac32aa36ab16fa
JakeLi22 <56609057+JakeLi22@users.noreply.github.com>
2021-05-05 10:00:25 -0400

    basic js skeleton
    to do:
    create html and css for next class

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,22 @@
 import { DOMSelectors } from "./DOM";
 import { genres } from "./genre";
 
-const key = "YOURKEYHERE";
+const key = "4553a2a9652947a58958783303dcd88b";
+
+const query = async function () {
+  try {
+    const response = await fetch(
+      `https://api.spoonacular.com/recipes/findByIngredients?ingredients=bread, cheese&apiKey=4553a2a9652947a58958783303dcd88b&ranking=2`
+    ); //fetch is searching for ingredients "bread, cheese"
+    const data = await response.json();
+    console.log(data);
+    /*data.forEach((recipe) =>{ //grid is the name of your section tag
+        DOMSelectors.grid.insertAdjacentHTML('beforeend', `COPY OVER YOUR HTML`)
+    })*/
+  } catch (error) {
+    console.log(error);
+    alert("something is wrong");
+  }
+};
+
+query();
