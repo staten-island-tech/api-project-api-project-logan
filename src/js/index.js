20caea656476e621ef4f6d10cedc2a71ea1d5d07
JakeLi22 <56609057+JakeLi22@users.noreply.github.com>
2021-05-26 21:51:08 -0400

    search works

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,7 @@ const key2 = "9df04583a2fe49a9a49d4112be770bd8";
 const query = async function () {
   try {
     const response = await fetch(
-      `https://api.spoonacular.com/recipes/findByIngredients?ingredients=bread, cheese&number=2&apiKey=${key2}&ranking=2`
+      `https://api.spoonacular.com/recipes/findByIngredients?ingredients${searchParams}&number=2&apiKey=${key}&ranking=2`
     ); //fetch is searching for ingredients "bread, cheese"
     const data = await response.json();
     // console.log(data);
@@ -31,56 +31,8 @@ const query = async function () {
     });
   } catch (error) {
     console.log(error);
-    alert("something is wrong");
+    alert("data from API could not be displayed");
   }
 };
 
 query();
-
-const listCreator = async function () {
-  try {
-    const response = await fetch(
-      `https://api.spoonacular.com/recipes/findByIngredients?ingredients=bread, cheese&number=2&apiKey=${key2}&ranking=2`
-    );
-    const data = await response.json();
-
-    data.forEach((ingredient) => {
-      let iteratorValue = 0;
-      const used = ingredient.usedIngredients;
-
-      const i = function ingredientIterator() {
-        if (ingredient.usedIngredientCount >= iteratorValue) {
-          iteratorValue + 1;
-          const newIngredient = document.createElement("li");
-          const newIngredientName = document.createTextNode(
-            "insert_ingredient_name"
-          );
-          newIngredient.appendChild(newIngredientName);
-          console.log(iteratorValue);
-          ingredientIterator();
-        } else {
-          console.log("done");
-        }
-      };
-
-      const ingredientList = used.forEach(() => {
-        //append to the create li
-        //  console.log("working");
-      });
-
-      //if ingredient count is above 0, then minus one, create an li, edit html of the li, forEach
-      // do the same for missedIngredient Count as well
-      //the editted html should be of the ingredient name
-
-      //console.log(missing);
-      //const missing = ingredient.missedIngredientCount;
-    });
-  } catch (error) {
-    console.log(error);
-    alert("something is wrong");
-  }
-};
-
-listCreator();
-
-//maybe organize using data[0]?
--- src/js/search.js
+++ src/js/search.js
@@ -1,16 +1,20 @@
 import { DOMSelectors } from "./DOM";
 
+const key = "4553a2a9652947a58958783303dcd88b";
+const key2 = "9df04583a2fe49a9a49d4112be770bd8";
+
 const listen = function () {
   DOMSelectors.searchForm.addEventListener("submit", function (e) {
     e.preventDefault();
+    DOMSelectors.grid.innerHTML = "";
     const searchParams = DOMSelectors.searchArea.value;
     const searchQuery = async function () {
       try {
         const response = await fetch(
-          `https://api.spoonacular.com/recipes/findByIngredients?ingredients=bread,cheese&apiKey=4553a2a9652947a58958783303dcd88b&ranking=2&language=en-US&query=${searchParams}`
+          `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${searchParams}&number=2&apiKey=${key2}&ranking=2`
         ); //fetch is searching for ingredients "bread, cheese"
         const data = await response.json();
-        console.log(data);
+        //console.log(data);
         data.forEach((recipe) => {
           //grid is the name of your section tag
           DOMSelectors.grid.insertAdjacentHTML(
